73218354f089984278443a83bec52d9d4a89bf89
9ruvivek <vivek9ru@gmail.com>
2015-12-05 00:40:48 +0530

    Finding Sum of Euler Totients till N, number of pairs with gcd d less than n

---
+++ Maths/Number_Of_Pairs_With_given_gcd.cpp
@@ -0,0 +1,72 @@
+/**********************************************************************************************************************************
+	
+						     Author  : Vivek Nynaru
+   						     Handle  : kevin11 
+						     Year    :  2015
+
+**********************************************************************************************************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+/******************************************************************************************************************************************
+      
+      Question: Find Sum of Euler Totient from 1 to N
+
+      Reference: https://codechef_shared.s3.amazonaws.com/upfiles/phisum.jpg
+
+      Complexity: Can be Considered to be atmost O(sqrt(N)) + Time to precompute phisum till a fixed limit(using normal way)
+
+******************************************************************************************************************************************/
+
+#define PHI 10000001
+
+ll phi[PHI];
+inline void init_phisum()
+{
+	for(ll i=1;i<PHI;i++)
+		phi[i]=(i&1)?i:i/2;
+	for(ll i=3;i<PHI;i+=2)
+		if(phi[i]==i)
+			for(ll j=i;j<PHI;j+=i)
+				phi[j]-=phi[j]/i;
+	for(int i=1;i<PHI;i++)
+		phi[i]+=phi[i-1];
+}
+inline ll phisum(ll n)
+{
+	if(n<PHI)
+		return phi[n];
+	ll ret=(n*(n+1))/2;
+	ll sq=2;
+
+	while(sq*sq<=n)
+	{
+		ret-=phisum(n/sq);
+		sq++;
+	}
+	sq=n/sq;
+	ll cnt;
+	while(sq)
+	{
+		cnt=n/sq-n/(sq+1); // Number of i's such that [n/i] is equal to sq
+		ret-=cnt*phi[sq];
+		sq--;
+	}
+	return ret;
+}
+int main()
+{
+	init_phisum();
+	int testcases;
+	cin >> testcases;
+	ll n,d;
+	// Number of pairs with gcd d less than n is equal to number of coprime pairs less than n/d
+	while(testcases--)
+	{
+		cin >> n >> d;
+		cout << phisum(n/d) << "\n";
+	}
+	return 0;
+}
